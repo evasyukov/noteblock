b093d768e2ed60d763c2374695744478839fb24a
evasyukov <egor.vasyuko1@gmail.com>
2023-08-31 15:31:58 +0700

    create "createNote" function and connect .css file

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const mainEl = document.querySelector('.main')
 
+// 1) написать функцию creteNote
+// 2) добавить кнопку и обработчик на добавление краточки
+
+function createNote(title, text) {
+    const noteEl = document.createElement('div')
+    noteEl.classList.add('note')
+    noteEl.innerHTML = `
+    <div class="note-header">
+        <p>${title}</p>
+        <div class="note-action>
+            <button class="note-edit><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
+        </div>
+    </div>
+    <p id="note-text">${text}</p>
+    `
+
+    return noteEl
+}
