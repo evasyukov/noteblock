012312ee2dcef50756e063c8844eb0b353e4d68c
evasyukov <egor.vasyuko1@gmail.com>
2023-08-31 19:50:41 +0700

    renamed the .css .js files.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const notesEl = document.querySelector('.notes')
 const addBtn = document.querySelector('.note-add')
 
+
 function createNote(title, text) {
     const noteEl = document.createElement('div')
     noteEl.classList.add('note')
@@ -8,15 +9,19 @@ function createNote(title, text) {
     <div class="note-header">
         <p>${title}</p>
         <div class="note-action>
-            <button class="note-edit><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
             <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
         </div>
     </div>
     <p id="note-text">${text}</p>
     `
 
+
+
     return noteEl
 }
+
+
 
 addBtn.addEventListener('click', (e) => {
     const el = createNote("Заголовок", "Ваш текст")
